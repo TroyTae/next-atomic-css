b0860177a93eadaeae9635f9980561ced0418a2e
태재영 <tjy970721@gmail.com>
2021-05-14 02:05:55 +0900

    dev mode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 module.exports = (nextConfig = {}) => ({
   ...nextConfig,
   webpack(config, options) {
-    config.module.rules.push({
-      test: /\.atomic\.css/,
-      use: [],
-    });
+    const { dev, isServer } = options;
+    const isClient = !isServer;
+
+    config.module.rules.splice(1, 1);
+
+    if (dev) {
+      config.module.rules.push({
+        sideEffects: true,
+        test: /(?<!\.module)\.css$/,
+        use: [isClient && "next-style-loader", "css-loader"].filter(Boolean),
+      });
+
+      config.module.rules.push({
+        sideEffects: true,
+        test: /\.module\.css$/,
+        use: [
+          isClient && "next-style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                exportOnlyLocals: isServer,
+              },
+            },
+          },
+        ].filter(Boolean),
+      });
+    }
 
     return typeof nextConfig.webpack === "function"
       ? nextConfig.webpack(config, options)
