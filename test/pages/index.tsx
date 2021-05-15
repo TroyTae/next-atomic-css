0191ced592e9f8e8c40270eb60715a14b5638824
태재영 <tjy970721@gmail.com>
2021-05-15 16:26:18 +0900

    sass test

--- test/pages/index.tsx
+++ test/pages/index.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import styles from "styles/Home.module.css";
 
 export default function Index() {
@@ -9,12 +8,6 @@ export default function Index() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
       </main>
-
-      <footer className={styles.footer}>
-        <Link href="">
-          <a>Go to sass test page</a>
-        </Link>
-      </footer>
     </div>
   );
 }
