ded0eb79746f5d3da0ef167cf1909900f192e0bf
태재영 <tjy970721@gmail.com>
2021-06-12 02:37:08 +0900

    feat: mangle

--- uglify-set/css-uglify.js
+++ uglify-set/css-uglify.js
@@ -1,73 +1,12 @@
-export const ALPHABET = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-const NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-// Maximum selector limit in IE10
-const LIMIT_COUNT = 2 ** 16;
-
-function Uglify(availableCharacters, isAvailableString) {
-  const maxIndex = availableCharacters.length;
-  let count = 0;
+function Mangle(characters, validate) {
+  const maxIndex = characters.length;
   let keys = [0];
   let set = {};
 
   return function (name) {
-    for (; !set[name]; ++count) {
-      const str = keys.map((key) => availableCharacters[key]).join("");
-      if (!isAvailableString || isAvailableString(str)) {
+    while (!set[name]) {
+      const str = keys.map((key) => characters[key]).join("");
+      if (validate(str)) {
         set[name] = str;
       }
       ++keys[0];
@@ -82,32 +21,82 @@ function Uglify(availableCharacters, isAvailableString) {
           }
         }
       });
-      if (count === LIMIT_COUNT) {
-        count = 0;
-        keys = [0];
-        set = {};
-      }
     }
     return set[name];
   };
 }
 
-const CSS_CHARACTERS = [
-  ...NUMBERS,
-  ...ALPHABET,
-  // https://www.w3.org/TR/CSS22/syndata.html#characters
-  "-",
-  "_",
-];
-
-function validateCss(str) {
-  return (
-    str.slice(0, 2) !== "--" &&
-    !NUMBERS.includes(str[0]) &&
-    !(str[0] === "-" && NUMBERS.includes(str[1]))
+function CssMangle() {
+  return Mangle(
+    [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+      "-",
+      "_",
+    ],
+    (str) =>
+      str.slice(0, 2) !== "--" &&
+      !NUMBERS.includes(str[0]) &&
+      !(str[0] === "-" && NUMBERS.includes(str[1]))
   );
-}
-
-export function CssUglify() {
-  return Uglify(CSS_CHARACTERS, validateCss);
 }
